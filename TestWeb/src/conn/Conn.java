45cb48f5372e8619f2bc37b2dbd878077adc905f
zhangyaqi <992321066@qq.com>
2019-12-10 10:05:48 +0800

    测试git

---
+++ TestWeb/src/conn/Conn.java
@@ -0,0 +1,15 @@
+package conn;
+
+import com.mysql.jdbc.Connection;
+
+public class Conn {
+	private String driver="com.mysql.jdbc.Driver";
+	private String url="mysql:jdbc//localhost:8080/we";
+	private String username="root";
+	private String password="root";
+	private Connection conn;
+	public Connection getConn() {
+		
+		return conn;
+	}
+}
