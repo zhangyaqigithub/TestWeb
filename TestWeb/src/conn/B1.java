837c3b35d48e421e0b15d0d9745ee30754a36137
zhangyaqi <992321066@qq.com>
2019-12-16 10:43:05 +0800

    在主分支上创建B1分支

---
+++ TestWeb/src/conn/B1.java
@@ -0,0 +1,11 @@
+package conn;
+/** 
+* @author 作者 Your-Name: ts03033
+* @version 创建时间：2019年12月16日 上午10:41:48 
+* 类说明 
+*/
+public class B1 {
+public static void main(String[] args) {
+	System.out.println("测试b1分支");
+}
+}
